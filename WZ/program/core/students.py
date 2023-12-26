41a39aff5b1a02e00dec46e0e7ed593fdec93797
gradgrind <mt.bothfeld@gmail.com>
2023-12-26 17:44:12 +0100

    Work on managing students data

--- WZ/program/core/students.py
+++ WZ/program/core/students.py
@@ -36,6 +36,8 @@ if __name__ == "__main__":
 
 ### +++++
 
+import json
+
 from core.db_access import (
     DB_TABLES,
     db_Table,
@@ -60,6 +62,13 @@ class Students(db_Table):
                 DB_FIELD_REFERENCE("Class", target = Classes.table),
                 DB_FIELD_TEXT("PID", unique = True),
                 DB_FIELD_TEXT("SORTNAME", unique = True),
+                DB_FIELD_TEXT("LASTNAME"),
+                DB_FIELD_TEXT("FIRSTNAMES"),
+                DB_FIELD_TEXT("FIRSTNAME"),
+                DB_FIELD_TEXT("DATE_BIRTH"),
+                DB_FIELD_TEXT("DATE_ENTRY"),
+                DB_FIELD_TEXT("DATE_EXIT"),
+                DB_FIELD_TEXT("BIRTHPLACE"),
                 DB_FIELD_TEXT("EXTRA"),
             )
             return True
@@ -77,11 +86,49 @@ class Students(db_Table):
             if data.Class.id == class_id:
                 students.append(data)
                 print("  --", data)
-#TODO: Process columns (e.g. EXTRA from json)
+#TODO: Process columns (e.g. EXTRA from json).
+# Note that writing to a row could be a bit tricky because of
+# converting to and from json ...
         return students
 
 
 DB_TABLES[Students.table] = Students
+
+
+def test_function_1(db):
+    fields = ["id", "GROUPS", "SEX", "HOME", "LEVEL", "DATE_QPHASE"]
+    for row in db.select(f'{",".join(fields)} from STUDENTS'):
+        data = {
+            fields[i]: val
+            for i, val in enumerate(row)
+            if val
+        }
+        id = data.pop("id")
+        edata = json.dumps(
+            data,
+            ensure_ascii = False,
+            separators = (',', ':')
+        )
+        print("  -->", edata)
+        db.update("STUDENTS", id, "EXTRA", edata)
+
+
+def test_function_2(db):
+    for row in db.select('id, EXTRA from STUDENTS'):
+        data = json.loads(row[1])
+        g = data.get("GROUPS")
+        if g:
+            glist = g.split()
+            #print(" &&&", glist)
+            data["GROUPS"] = glist
+            edata = json.dumps(
+                data,
+                ensure_ascii = False,
+                separators = (',', ':')
+            )
+            print("  -->", row[0], edata)
+            db.update("STUDENTS", row[0], "EXTRA", edata)
+
 
 
 # --#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#
@@ -90,10 +137,14 @@ if __name__ == "__main__":
     from core.basic_data import get_database
     db = get_database()
 
-    #print("\n?create-sql:", Subjects.sql_create_table())
+    #print("\n?create-sql:", Students.sql_create_table())
 
     students = Students(db)
     students.student_list(21)
+
+
+
+
 #    for s, index in subjects.id2index.items():
 #        print(f"\n  {s}: {index:02d}/{subjects.records[index]}")
 
